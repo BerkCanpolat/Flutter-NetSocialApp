0cf2cc18dc9d904ad3945585c4181df80a3bcaa9
BerkCanpolat <canpolatberk19@gmail.com>
2023-06-23 18:38:01 +0300

    Update Profil Eklendi

--- flutter_netsocialapp/lib/Provider/provider.dart
+++ flutter_netsocialapp/lib/Provider/provider.dart
@@ -1,5 +1,10 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_netsocialapp/Firebase/auth.dart';
+import 'package:flutter_netsocialapp/Firebase/storage.dart';
+import 'package:flutter_netsocialapp/constants/constant.dart';
 import 'package:flutter_netsocialapp/model/userModel.dart';
 
 class ProviderNet with ChangeNotifier {
@@ -8,9 +13,36 @@ class ProviderNet with ChangeNotifier {
 
   UserModel? get getUserProvider => _user;
 
-  Future<void> userDetailsProvider() async{
+  Future<void> userDetailsProvider() async {
     UserModel? user = await _authService.userDetailsFirebase();
     _user = user;
+    notifyListeners();
+  }
+
+  Future<void> updateUserProfil(
+      Uint8List file, UserModel userModel, BuildContext context) async {
+    if (file == null) {
+      showLoaderDialog(context);
+      _user = userModel;
+      await FirebaseFirestore.instance
+          .collection("Users")
+          .doc(_user?.uid)
+          .set(_user!.toJson());
+      Navigator.of(context, rootNavigator: true).pop();
+      Navigator.of(context).pop();
+    } else {
+      showLoaderDialog(context);
+      String imageUrl =
+          await StorageMethod().storageAdd("UsersProfilePhoto", file, false);
+      _user = userModel.copyWith(userPhoto: imageUrl);
+      await FirebaseFirestore.instance
+          .collection("Users")
+          .doc(_user?.uid)
+          .set(_user!.toJson());
+      Navigator.of(context, rootNavigator: true).pop();
+      Navigator.of(context).pop();
+    }
+    cherryMessage("Profil Güncellendi!", context);
     notifyListeners();
   }
 }
